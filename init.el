a8d2f4f43c52373cbe357931aa71e2b86d1e006f
Wang Haiqiang <haiqiang.wang@uisee.com>
2020-01-24 15:21:17 +0800

    Fix ECB bug which reuses the same frame for minibuffer and completion

--- init.el
+++ init.el
@@ -249,6 +249,12 @@ There are two things you can do about this warning:
 
 (require 'ecb)
 (global-set-key (kbd "<f5>") 'ecb-minor-mode)
+(defun display-buffer-at-bottom--display-buffer-at-bottom-around (orig-fun &rest args)
+"Bugfix for ECB: cannot use display-buffer-at-bottom', call display-buffer-use-some-window' instead in ECB frame."
+(if (and ecb-minor-mode (equal (selected-frame) ecb-frame))
+(apply 'display-buffer-use-some-window args)
+(apply orig-fun args)))
+(advice-add 'display-buffer-at-bottom :around #'display-buffer-at-bottom--display-buffer-at-bottom-around)
 
 (defun toggle-window-split ()
   (interactive)
