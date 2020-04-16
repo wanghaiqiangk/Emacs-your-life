920bc9d7ae9f6a004d09d4597c86f5731a581a27
haiqiang.wang <wanghqno1@outlook.com>
2020-04-16 22:40:39 +0800

    Fix wrong mirror replacement

--- user-config/init-packages.el
+++ user-config/init-packages.el
@@ -18,13 +18,10 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (setq package-archives nil)
   (add-to-list 'package-archives (cons "melpa" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")) t)
   (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    ;; (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))
     (add-to-list 'package-archives (cons "gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
     ))
 
