864b7025fa9993ea94fb6f84e22bea582482b294
Wang Haiqiang <haiqiang.wang@uisee.com>
2020-04-16 15:14:24 +0800

    Replace with tuna elpa mirrors

--- user-config/init-packages.el
+++ user-config/init-packages.el
@@ -19,11 +19,14 @@ There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")) t)
   ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+    ;; (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+    ))
 
 ;; Packages list in need
 (setq my-package-list
