a2d27ca6c3873654b80e82ef0839a8daf82e2843
Wang Haiqiang <haiqiang.wang@uisee.com>
2020-04-21 13:33:34 +0800

    bugfix(perhaps) tramp-file-name-struct didn't match

--- user-config/basic-config.el
+++ user-config/basic-config.el
@@ -137,6 +137,23 @@
 (global-set-key (kbd "C-c f r") 'flx-isearch-backward)
 (global-set-key (kbd "C-c h m") 'helm-man-woman)
 
+;;; Tramp bug maybe
+;;
+(defun ido-remove-tramp-from-cache nil
+  "Remove any TRAMP entries from `ido-dir-file-cache'.
+This stops tramp from trying to connect to remote hosts on Emacs startup,
+which can be very annoying."
+  (interactive)
+  (setq ido-dir-file-cache
+        (cl-remove-if
+         (lambda (x)
+           (string-match "/\\(rsh\\|ssh\\|telnet\\|su\\|sudo\\|sshx\\|krlogin\\|ksu\\|rcp\\|scp\\|rsync\\|scpx\\|fcp\\|nc\\|ftp\\|smb\\|adb\\):" (car x)))
+         ido-dir-file-cache)))
+;; redefine `ido-kill-emacs-hook' so that cache is cleaned before being saved
+(defun ido-kill-emacs-hook ()
+  (ido-remove-tramp-from-cache)
+  (ido-save-history))
+
 (provide 'basic-config)
 
 ;;; basic-config.el ends here
