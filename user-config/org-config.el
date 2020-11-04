318d587d939f27d45232aa4e263434463cd2254c
Wang Haiqiang <haiqiang.wang@uisee.com>
2020-11-04 12:54:14 +0800

    Add function to quickly open default agenda file

--- user-config/org-config.el
+++ user-config/org-config.el
@@ -15,6 +15,13 @@
   (setq org-special-ctrl-a/e t)
   (setq org-special-ctrl-k nil))
 
+(defun my/open-my-agenda-file ()
+  "Open agenda file."
+  (interactive)
+  (find-file "~/.emacs.d/org/agenda.org"))
+
+(global-set-key (kbd "C-c a g") 'my/open-my-agenda-file)
+
 (eval-after-load "org"
   '(my/org-settings))
 
